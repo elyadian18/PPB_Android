686e70c89f3956886656c3622dda149249838f7f
elyadian18 <elyadian18@gmail.com>
2021-10-06 07:31:59 +0700

    Update

---
+++ FragmentEx_11653/app/src/main/java/com/udinus/fragmentex_11653/firstFragment.java
@@ -0,0 +1,30 @@
+package com.udinus.fragmentex_11653;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+public class firstFragment extends Fragment {
+    View view;
+    Button FirstButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        view = inflater.inflate(R.layout.fragment_first, container, false);
+        FirstButton = (Button) view.findViewById(R.id.FirstButton);
+        FirstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Fragment Pertama", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
+    }
+
+}
---
+++ Konversi_Desain/dtschapter03-starter-master/app/src/main/java/polinema/ac/id/dtschapter03_starter/WelcomeSlideAssign_11653.java
@@ -0,0 +1,25 @@
+package polinema.ac.id.dtschapter03_starter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeSlideAssign_11653 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_slide_assign11653);
+    }
+    public void clickGetStarted (View view){
+        Intent i = new Intent(WelcomeSlideAssign_11653. this, WelcomeSlideSuperhero_11653.class);
+        startActivity(i);
+
+    }
+
+    public void clickLogin(View view){
+        Intent i =new Intent(WelcomeSlideAssign_11653.this, WelcomeBack_11653.class);
+        startActivity(i);
+    }
+}
